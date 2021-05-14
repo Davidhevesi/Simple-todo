093e3cd9d66a9d0f571358528a288f3a604846a6
David Hevesi <davidhevesi2008@gmail.com>
2021-05-14 14:26:06 -0400

    Added authentication action in store

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,10 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/amplify.js', mode: 'client' }],
+  plugins: [
+    { src: '~/plugins/amplify.js', mode: 'client' },
+    
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
